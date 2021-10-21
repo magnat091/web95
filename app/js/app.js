5b53837958f0a46e3ac3ead9f1dde7c2cfa4d465
agalaev <symbyozis@gmail.com>
2021-10-21 12:11:39 +0300

    finish

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     $('.slider').slick({
         slidesToShow: 3,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
     })
 
+    $(document).on('click', '.main-btn', function (e) {
+        $(this).toggleClass('active');
+        $('.btn-cont').toggleClass('active');
+        $('.nav--mobile').slideToggle()
+    });
 })
